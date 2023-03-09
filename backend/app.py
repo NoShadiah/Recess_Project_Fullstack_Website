119f9c02fc76183accd0133ef280eb6ceb953fb7
NoShadiah <nabawangashadiah20@gmail.com>
2023-03-09 09:45:45 +0300

    Working on the backend

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from backend import create_app, db
+from flask_migrate import Migrate
+
+# from backend.profile.model import Profile
+
+app = create_app('development')
+migrate = Migrate(app, db)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db)
---
+++ backend/models/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from config import config
+from backend.db import db
+
+
+def create_app(config_name): #Application Factory Funciton
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    app.config.from_pyfile("../config.py")
+
+
+    db.init_app(app)
+
+    @app.route('/')
+    def greet():
+        return "Hallo there"
+    #import the blueprints
+    
+    # from backend.books.controllers import books
+
+    # #registering blue prints
+    #app.register_blueprint()
+    # app.register_blueprint(books)
+
+    return app
